5b91224d0cc39f470914c1b5c13654b747200069
csullivan <csulli1@umbc.edu>
2016-05-04 10:39:42 -0400

    take 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,16 +8,18 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Hello welcome to Java accounting!\nPlease enter your first name:\n");
+        System.out.println("Hello welcome to Java accounting!\nPlease enter your first name:");
         String firstName = input.nextLine();
         String firstName2 = firstName.toUpperCase();
 
-        System.out.println("Please eneter your last name:\n");
+        System.out.println("Please eneter your last name:");
         String lastName = input.nextLine();
         String lastName2 = lastName.toLowerCase();
 
         int randomNumber = 10 + (int) (Math.random() * 90);
         char a = firstName2.charAt(firstName2.length() - 1);
+
+        System.out.println("Your identifier is " + a + randomNumber + lastName2.substring(0, 3));
 
 
 
