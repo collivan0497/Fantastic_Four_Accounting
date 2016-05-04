ab264e3bc7da89cb021e354245f126f181b06636
csullivan <csulli1@umbc.edu>
2016-05-04 10:38:17 -0400

    trying to start over...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello welcome to Java accounting!\nPlease enter your first name:\n");
+        String firstName = input.nextLine();
+        String firstName2 = firstName.toUpperCase();
+
+        System.out.println("Please eneter your last name:\n");
+        String lastName = input.nextLine();
+        String lastName2 = lastName.toLowerCase();
+
+        int randomNumber = 10 + (int) (Math.random() * 90);
+        char a = firstName2.charAt(firstName2.length() - 1);
+
+
+
+
+        // write your code here
     }
 }
