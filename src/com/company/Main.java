413232fc27c3dec1523d87c94794baa9d4a01a81
csullivan <csulli1@umbc.edu>
2016-05-05 12:09:31 -0400

    Do while loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,8 @@ public class Main {
     private static double[] weeklyNet = new double[5];
     private static double[] weeklyExpenses = new double[50];
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         double grossTOTAL = 0;
         double grossSAVING = 0;
@@ -37,42 +38,33 @@ public class Main {
 
         System.out.println("This program will be able to take 1 month (4 weeks) of your income and " +
                 "allow you to see how much you are actually taking home at the end of the month. \n");
-
-        questions();
-
-
-
-
-
-        for( int i =0; i < weeklyGross.length; i++){
-            grossTOTAL += weeklyGross[i];
-        }
-        for(int i = 0; i < weeklySavings.length; i++){
-            grossSAVING += weeklySavings[i];
-        }
-        for(int i = 0; i < weeklyNet.length; i++){
-            grossNET += weeklyNet[i];
-        }
-        System.out.println("This month's gross is " + currFormat.format(grossTOTAL) + "\n This month you saved " +
-                currFormat.format(grossSAVING) + "\n This month you netted " + currFormat.format(grossNET));
-
-
-        System.out.println();
-        System.out.println(" Would you like to enter another month's information?\n If yes, press 'y' or 'Y'. If no, press 'n' or 'N'");
-        String quit1 = input.nextLine();
-        quit = quit1.charAt(0);
-
-        if(quit=='n' || quit=='N'){
-            System.out.println("Thanks for using this program. Goodbye");
-        } else {
+        // "Kyong Kang" added doWhile loop so the program keep repeating until n/N is pressed
+        do
+        {
             questions();
-        }
+            for (int i = 0; i < weeklyGross.length; i++) {
+                grossTOTAL += weeklyGross[i];
+            }
+            for (int i = 0; i < weeklySavings.length; i++) {
+                grossSAVING += weeklySavings[i];
+            }
+            for (int i = 0; i < weeklyNet.length; i++) {
+                grossNET += weeklyNet[i];
+            }
+            System.out.println("This month's gross is " + currFormat.format(grossTOTAL) + "\n This month you saved " +
+                    currFormat.format(grossSAVING) + "\n This month you netted " + currFormat.format(grossNET));
 
 
+            System.out.println();
+            System.out.println(" Would you like to enter another month's information?\n If yes, press 'y' or 'Y'. If no, press 'n' or 'N'");
+            String quit1 = input.nextLine();
+            quit = quit1.charAt(0);
+        }   while (quit !='n' && quit !='N');
+        System.out.println("Thanks for using this program. Goodbye");
     }
 
-
-    public static void questions(){
+    public static void questions()
+    {
         for (int i = 0; i < 4; i++) {
 
             Scanner input1 = new Scanner(System.in);
@@ -97,12 +89,6 @@ public class Main {
                     "\nFor example, if you spent to save $100.00, enter '100.00'");
             weeklyExpenses[i] = input1.nextDouble();
             weeklyNet[i] -= weeklyExpenses[i];
-
-
-
         }
-
     }
-
-
 }
